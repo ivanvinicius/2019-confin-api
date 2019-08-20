7a352c830bdc7a51dfddd76742d2c21c93fe1f68
ivan <ivanvinicius@github.com>
2019-08-20 10:18:55 -0300

    Feature correção na migration fk-estado-cidade

--- src/database/migrations/20190819185220-fk-estado-cidade.js
+++ src/database/migrations/20190819185220-fk-estado-cidade.js
@@ -2,7 +2,6 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.addColumn('cidades', 'est_sigla', {
       type: Sequelize.STRING(2),
-      primaryKey: true,
       references: { model: 'estados', key: 'est_sigla' },
       onUpdate: 'NO ACTION',
       onDelete: 'NO ACTION',
